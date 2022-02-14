2d7fb626ddaa34472699b1ffc0762bed7d0f6eeb
TheEmon <mdemrannaziremon@gmail.com>
2022-02-14 16:27:35 +0600

    Counter-App (without using for each loop)

--- js/app.js
+++ js/app.js
@@ -1,30 +1,34 @@
-let counter = 0;
 
-// select value and buttons
+let count = 0;
 
-const value = document.querySelector('#value');
-const btns = document.querySelectorAll('.btn');
+const value = document.getElementById('value');
+const increase = document.getElementById('increase');
+const decrease = document.getElementById('decrease');
+const reset = document.getElementById('reset');
 
-btns.forEach(function (btn) {
-    btn.addEventListener("click", function(e){
-        const styles = e.currentTarget.classList;
-        if (styles.contains('decrease')) {
-            counter--
-        } else if(styles.contains('increase')) {
-            counter++;
-        } else {
-            counter = 0;
-        }
 
-        if(counter < 0) {
-            value.style.color = 'red';
-        } 
-        if (counter > 0) {
-            value.style.color = 'green'
-        }
-        if(counter === 0) {
-            value.style.color = 'black'
-        }
-        value.textContent = counter;
-    })
+increase.addEventListener('click', function(){
+	count++;
+    value.textContent = count
+});
+
+decrease.addEventListener('click', function() {
+    count--;
+    value.textContent = count;
+});
+
+reset.addEventListener('click', function() {
+    count = 0;
+    value.textContent = count;
 })
+
+if(counter < 0) {
+    value.style.color = 'red';
+} 
+if (counter > 0) {
+    value.style.color = 'green'
+}
+if(counter === 0) {
+    value.style.color = 'black'
+}
+value.textContent = counter;
